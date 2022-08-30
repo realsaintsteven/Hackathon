b0f6ecdf6f104a1e7d43294f9b2e2fb534b5c382
Oladeji Stephen <oladejisteven@gmail.com>
2022-08-30 04:46:50 +0100

    seeders

--- database/seeders/loginAdminSeeder.php
+++ database/seeders/loginAdminSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class adminLoginSeeder extends Seeder
+class loginAdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.

f286356e6b0aca1154840ac8fbd6b4e05d12400d
Oladeji Stephen <oladejisteven@gmail.com>
2022-08-30 04:48:18 +0100

    admin seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RolePermissionsTableSeeder::class);
+      //  $this->call(RolePermissionsTableSeeder::class);
 
         $this->call(loginAdminSeeder::class);
 

cdde97ebc678df6d7bda5a96de7eacd8047f4ee0
Oladeji Stephen <oladejisteven@gmail.com>
2022-08-30 04:45:10 +0100

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolePermissionsTableSeeder::class);
 
+        $this->call(loginAdminSeeder::class);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
---
+++ database/seeders/loginAdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class adminLoginSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0');
+            DB:: table('admins') ->insert([
+            "email" => "admin@gmail.com",
+            "password" =>'password',
+            'uid' => 1,
+            'name' => 'Admin System',
+            
+           
+           ]);
+      
+    }
+}
